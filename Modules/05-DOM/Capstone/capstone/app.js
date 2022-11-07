c17017f1675e4b6f67b2aabed60f501eed4e7a58
eekinw <wongeekinn@gmail.com>
2022-11-07 15:35:32 +0800

    DOM Capstone

---
+++ Modules/05-DOM/Capstone/capstone/app.js
@@ -0,0 +1,16 @@
+// Select the header element
+                
+
+
+
+
+
+// Event listener: Mouse move                
+// When user hovers around the top area, show the header                
+                
+
+
+
+
+// Event listener: Scroll                
+// When user scroll after a certain height, hide the header, else show the header
